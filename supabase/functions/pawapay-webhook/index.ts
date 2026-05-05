a2d61ddb20ec93311e39cdf7a096dc081dc591d2
tychiquelesambo0 <tychiquelesambo0@users.noreply.github.com>
2026-05-05 17:22:17 +0200

    fix: correct import path for pawapay-webhook

--- supabase/functions/pawapay-webhook/index.ts
+++ supabase/functions/pawapay-webhook/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "@std/http/server";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { jsonResponse } from "../_shared/http.ts";
 import { generateSecure4DigitPin } from "../_shared/pin.ts";
 import { createServiceRoleClient } from "../_shared/supabaseClient.ts";
